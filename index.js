b66865a37c926a72a5944979c2488374a2bfc430
vthourte <valentinthourte@gmail.com>
2024-05-13 22:14:14 -0300

    Agrego click en tabla para insertar datos en formulario

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 import { AnuncioAutomovil } from "./anuncio-automovil.js";
 import { leer_async, escribir_async, borrar_async } from "./local-storage-async.js";
 
-
-const items = []
 const KEY_STORAGE = "autos"
+const items = []
 document.addEventListener("DOMContentLoaded", () => {
     onInit();
+    document.addEventListener("click",(e) =>  handleClick(e));
 });
 
 
 const formulario = document.getElementById("formAutos");
+
+
+function handleClick(e) {
+    if (e.target.matches("td")) { // Si clickeo una celda
+        let id = e.target.parentNode.dataset.id; // Obtener el id de la fila
+        console.log(id);
+        console.log(items);
+        let anuncio = items.filter(a => a.id == id)[0];
+        console.log(anuncio);
+        cargarFormulario(
+            anuncio.titulo,
+            anuncio.descripcion,
+            anuncio.precio,
+            anuncio.puertas,
+            anuncio.kms,
+            anuncio.potencia,
+            anuncio.transaccion
+        )
+    }
+}
+function cargarFormulario(...datos) {
+    formulario.querySelector("#titulo").value = datos[0];
+    formulario.querySelector("#descripcion").value = datos[1];
+    formulario.querySelector("#precio").value = datos[2];
+    formulario.querySelector("#puertas").value = datos[3];
+    formulario.querySelector("#kms").value = datos[4];
+    formulario.querySelector("#potencia").value = datos[5];
+    if (datos[6] == "Venta") {
+
+        formulario.querySelector("#rVenta").checked = true;
+        formulario.querySelector("#rAlquiler").checked = false;
+    }
+    else {
+        formulario.querySelector("#rVenta").checked = false;
+        formulario.querySelector("#rAlquiler").checked = true;
+    }
+    
+}
 
 async function onInit() {
     await loadItems();
@@ -30,7 +68,7 @@ async function loadItems() {
             obj.precio,
             obj.puertas,
             obj.kms,
-            obj,potencia
+            obj.potencia
         );
     
         items.push(model);
@@ -38,7 +76,7 @@ async function loadItems() {
 }
 
 function escucharFormulario() {
-    formulario.addEventListener("submit", (e) => {
+    formulario.addEventListener("submit", async (e) => {
         e.preventDefault();
 
         const id = obtenerNuevoId();
@@ -57,7 +95,7 @@ function escucharFormulario() {
         items.push(model);
 
         const str = JSON.stringify(items);
-        escribir_async(KEY_STORAGE, str);
+        await escribir_async(KEY_STORAGE, str);
 
         actualizarFormulario();
         rellenarTabla();
@@ -71,13 +109,18 @@ function obtenerNuevoId() {
 
 function obtenerValorRadio() {
     const radios = document.querySelectorAll('input[name="transaccion"]');
-    let selectedValue;
+    let selectedText = '';
+
     radios.forEach(radio => {
         if (radio.checked) {
-            selectedValue = radio.value;
+            const label = document.querySelector(`label[for="${radio.id}"]`);
+            if (label) {
+                selectedText = label.textContent;
+            }
         }
     });
-    return selectedValue;
+
+    return selectedText;
 }
 
 function actualizarFormulario() {
@@ -100,10 +143,15 @@ function rellenarTabla() {
         let nuevaFila = document.createElement("tr");
 
         celdas.forEach((celda) => {
-            let nuevaCelda = document.createElement("td");
-            nuevaCelda.textContent = item[celda];
+            if (celda == "id") {
+                nuevaFila.setAttribute("data-id", item[celda])
+            }
+            else {
+                let nuevaCelda = document.createElement("td");
+                nuevaCelda.textContent = item[celda];
+                nuevaFila.appendChild(nuevaCelda);
+            }
 
-            nuevaFila.appendChild(nuevaCelda);
         });
 
         tbody.appendChild(nuevaFila);
